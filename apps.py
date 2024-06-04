bfb2023d37f15d00c6e00d0cb50a5ea6a3645b6e
Ben Juo <143736446+rrrr80@users.noreply.github.com>
2024-06-04 00:20:06 -0400

    Update apps.py

--- apps.py
+++ apps.py
@@ -4,22 +4,64 @@ from flask import Flask, render_template, request
 import imaplib
 import email
 app = Flask(__name__)
+# ^ import nessecary files, flask requires pip installation 
 
-
+# Flask procedure to execute different programs
 @app.route("/")
 def index():
     return render_template("index.html")
 
+@app.route("/summarize")
+def summarize():
+    # retrieve message
+    final_messages = main()
+    # determine if there is an error, if so divert to error page
+    if len(final_messages) == 0:
+        return render_template("error.html")
+    # Summerizing procedure
+    for i in range(len(final_messages)):
+            final_messages[i] = "HAH"
+    # Format message to be displayed
+    final_messages = ' '.join(final_messages)
+    global senders 
+    senders = " ".join(senders)
+    global subjects
+    subjects = " ".join(subjects)
 
-@app.route("/greet")
-# ^ import nessecary files, yaml requires pip installation
+
+    return render_template("summarize.html",  message=final_messages, subject=subjects, sender=senders)
+            
+
+@app.route("/categorize")
+   
+def categorize():
+    # retrieve message
+    final_messages = main()
+    # determine if there is an error, if so divert to error page
+    if len(final_messages) == 0:
+        return render_template("error.html")
+
+    # format message for display
+    final_messages = ' '.join(final_messages)
+    catagories = tagMail(final_messages)
+
+    global senders 
+    senders = " ".join(senders)
+    global subjects
+    subjects = " ".join(subjects)
+    
+    
+
+    return render_template("categorize.html", message=final_messages, subject=subjects, sender=senders, catagory = catagories)
+
+
+
+
+
+
+
 def main():
-    # Open file with credentials and set it to a variable
-    # f = open("credentials.csv", "r")
-    # my_credentials = []
-    # for row in f:
-    #     my_credentials.append(row)
-    # my_credentials = f
+    # Login credentials, requires manual input as of now
     my_credentials = ["benson.computer.project@gmail.com", "ztwf bpcw bsdq qfuo"]
 
     # Set variables for login
@@ -42,10 +84,12 @@ def main():
     value = 'is:starred'
     msgs = searchMail(my_mail, key, value)
     
+    # Make email contents ready for viewing 
     final_messages = printMail(msgs)
-    final_messages = ' '.join(final_messages)
+    return (final_messages)
 
-    return render_template("greet.html", message=final_messages)
+
+
 
 
 
@@ -54,23 +98,56 @@ def searchMail(my_mail, key, value):
     _, data = my_mail.search(None, key, value)
     # Assign flagged emails that match parameters to an id list.
     mail_id_list = data[0].split()
+
+        
     msgs = []
     # Search for message contents within each email, '(RFC822)' is a standard signiture for digital mail, append it to msg
     for num in mail_id_list:
         typ, data = my_mail.fetch(num, '(RFC822)')
         msgs.append(data)
+    if len(mail_id_list) > 1:
+        msgs = ["ERROR"]
     return msgs
 
-
 def tagMail(plain_text):
+        # Define key words to search for
+        # --------------------------------INPUT CUSTOM WORDS------------------------------
+        # Match the theme ["word", "word", "word"], in all lowercase
+        # replace "WORD" with topic you want emails to fall in
+        words1 = ["meeting", "business", "boss"]
+        words2 = ["lol", "fr", "yo",]
+        words3 = []
+        words4 = []
+        words5 = []
+        # Convert to lower case for easy search
+        plain_text.lower()
         textList = plain_text.split()
+        # Search, if match found return topic
         for item in textList:
-            if item == "the":
-                return True
-
+            if item in words1:
+                return "Formal"
+            elif item in words2:
+                return "Informal"
+            elif item in words3:
+                return "WORD"
+            elif item in words4:
+                return "WORD"
+            elif item in words5:
+                return "WORD"
+            else:
+                return "None"
+            
+    
 
 def printMail(msgs):
     # Print the message
+
+    # Assign global variables to be transfered to main function                 
+    global subjects
+    subjects = []
+    global senders 
+    senders = []
+
     final_messages = []
     for msg in msgs[::-1]:
         for response_part in msg:
@@ -79,7 +156,12 @@ def printMail(msgs):
                 # print("_________________________________________")
                 # print("subj:", my_msg['subject'])
                 # print("from:", my_msg['from'])
-                print("body:")
+                # print("body:")
+
+                senders.append(my_msg['from'])
+
+                subjects.append(my_msg["subject"])
+                # subjects.append(my_msg["subject"])
                 # What does this even mean???
                 plain_text = ""
                 html_text = ""
@@ -88,11 +170,8 @@ def printMail(msgs):
                         plain_text = part.get_payload(decode=True).decode()
                     elif part.get_content_type() == 'text/html':
                         html_text = part.get_payload(decode=True).decode()
-                if tagMail(plain_text) == True:
-                    print("check", "")
-
                 if plain_text:
-                    final_messages.append(plain_text +"\n")
+                    final_messages.append(plain_text)
                 else:
-                    final_messages.append(html_text + "\n")
+                    final_messages.append(html_text)
     return(final_messages)
